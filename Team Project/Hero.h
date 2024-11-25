82df53c4e120254e6a984ef3352085616f2f3390
gpradof1 <71532460+gpradof1@users.noreply.github.com>
2024-11-25 18:58:49 -0500

    Hero.h additional functions (not tested yet)

--- Team Project/Hero.h
+++ Team Project/Hero.h
@@ -11,20 +11,35 @@
 #include "yspng.h"
 #include "fssimplewindow.h"
 #include "Enemy.h"
+#include "Map.h"
+#include "ItemManage.h"
+
 
 class Hero
 {
+private:
+    int tileX;
+    int tileY;
 public:
     Hero(); //constructor. Sets values for pixelX, pixelY, direction, HP.
-    int tileX; //x-position of tile
-    int tileY; //y-position of tile
+    //int tileX; //x-position of tile
+    //int tileY; //y-position of tile
     int pixelX; //number of pixels of hero pictures in x direction (40)
     int pixelY; //number of pixels of hero pictures in y direction (40)
     int direction; //number from 0 to 3, keeps track of where enemy is facing (0 = up, 1 = right, 2 = left, 3 = down)
     int gameTime;
     double HP; //health = 100
-    void Draw(const unsigned char front[], const unsigned char right[], const unsigned char left[], const unsigned char back[]); //draws sprites. the char lists are the .rgba lists for each sprite
-    void Move(int key, Enemy rabbit[]); //moves character based on keyboard input and checks collision with rabbits
+    int GetPlayerRow();
+    int GetPlayerCol();
+    bool itemHeld;
+    int range;
+    int damage;
+    int health;
+    void PickItemIfAny(); //for testing use an item instant, otherwise there is no argument.
+    void Attack(Enemy rabbit[]); //attacks enemy when space key is pressed in gamecontroller
+    void SpawnPlayer(int tileX, int tileY);
+    void Draw(const unsigned char front[], const unsigned char right[], const unsigned char left[], const unsigned char back[]) const; //draws sprites. the char lists are the .rgba lists for each sprite. is called constantly in main loop
+    void Move(int key, Enemy rabbit[],Map map); //moves character based on keyboard input and checks collision with rabbits. 
 };
 
 #endif
