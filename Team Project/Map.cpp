6de2865788aadc6ed37403a764a45fa42c1a66c4
PetrMundo <68665135+PetrMundo@users.noreply.github.com>
2024-11-25 23:49:44 -0500

    Update Map.cpp
    The end functions are in case you want to generate a random maze and write it to a text file in the main function, taken from Shang-Chun's algorithm. That file would then have to be altered a bit and given to Map's LoadFromFile function.
    
    GetItem still needs to be sorted out with the Item class. I'm trying to return a copy of gun, healthPotion, and sword based on Tile.h, but it's not working as expected.

--- Team Project/Map.cpp
+++ Team Project/Map.cpp
@@ -96,3 +96,136 @@ int Map::GetPlayerCol() const
 {
     return playerCol;
 }
+
+void Map::SpawnPlayer(int x, int y)
+{
+    tiles[y][x] = Tile(x * TILE_SIZE, y * TILE_SIZE, PLAYER);
+}
+
+Item Map::GetItem(int x, int y)
+{
+    return tiles[y][x].GetItem();
+}
+
+//
+// This part is if you want to generate a maze randomly and write it to a text file.
+//
+
+#include <stdio.h>
+#include <iostream>
+#include <queue>
+#include <vector>
+#include <ctime>
+#include <cstdlib>
+#include "fssimplewindow.h"
+//#include "yssimplesound.h"
+#include <fstream>
+
+using namespace std;
+
+const int windowWidth = 800;
+const int windowHeight = 800;
+// Must be odd for maze generation
+const int mazeWidth = 19;
+const int mazeHeight = 19;
+// Set the size so that maze width/height * Size <= window width/height
+const int cellSize = 20;
+
+// Maze grid: 0 for spaces, 1 for walls, 2 for current path,  3 for start/end
+int maze[mazeHeight][mazeWidth];
+
+// Directions
+const int directions[4][2] = {
+    {-1, 0},  // Up
+    {0, 1},   // Right
+    {1, 0},   // Down
+    {0, -1}   // Left
+};
+
+struct Node {
+    int x, y;
+
+    bool operator==(const Node& other) const {
+        return x == other.x && y == other.y;
+    }
+};
+
+void GenerateMaze() {
+    // Initialize the maze with walls
+    for (int i = 0; i < mazeHeight; ++i) {
+        for (int j = 0; j < mazeWidth; ++j) {
+            maze[i][j] = 1; // Wall
+        }
+    }
+
+    // Starting position
+    int startX = 1;
+    int startY = 1;
+
+    // Define the stack for backtracking
+    vector<pair<int, int>> cellStack;
+    cellStack.push_back({ startX, startY });
+    // Mark the starting cell as passage
+    maze[startY][startX] = 0;
+
+    while (!cellStack.empty()) {
+        int x = cellStack.back().first;
+        int y = cellStack.back().second;
+
+        // Find unvisited neighbors
+        vector<pair<int, int>> neighbors;
+
+        for (const auto& dir : directions) {
+            int nx = x + dir[1] * 2;
+            int ny = y + dir[0] * 2;
+
+            if (nx > 0 && nx < mazeWidth - 1 && ny > 0 && ny < mazeHeight - 1) {
+                if (maze[ny][nx] == 1) {
+                    neighbors.push_back({ nx, ny });
+                }
+            }
+        }
+
+        if (!neighbors.empty()) {
+            // Choose a random neighbor
+            int randIndex = rand() % neighbors.size();
+            auto nextCell = neighbors[randIndex];
+
+            // Remove the wall between current cell and chosen cell
+            int wallX = x + (nextCell.first - x) / 2;
+            int wallY = y + (nextCell.second - y) / 2;
+            maze[wallY][wallX] = 0;
+            maze[nextCell.second][nextCell.first] = 0;
+
+            // Push the chosen cell to the stack
+            cellStack.push_back(nextCell);
+        }
+        else {
+            // Backtrack
+            cellStack.pop_back();
+        }
+    }
+}
+
+// Function to write the maze to a text file
+void MazeToFile(const int maze[mazeHeight][mazeWidth], const std::string& filename)
+{
+    std::ofstream file(filename);
+    if (file.is_open()) {
+        for (int i = 0; i < mazeHeight; ++i) {
+            for (int j = 0; j < mazeWidth; ++j) {
+                file << maze[i][j];
+                if (j < mazeWidth - 1) {
+                    file << " ";
+                }
+            }
+            file << std::endl;
+        }
+        file.close();
+        std::cout << "Maze saved to " << filename << std::endl;
+    }
+    else
+    {
+        std::cerr << "Unable to open file" << std::endl;
+    }
+}
