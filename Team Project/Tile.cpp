da13061162aba1a7e034685b509c8fd64d6281f4
PetrMundo <68665135+PetrMundo@users.noreply.github.com>
2024-11-25 23:55:28 -0500

    Update Tile.cpp
    Tile still needs to communicate with the Player and Enemy classes to Draw them - right now they take arguments that I'm not sure how to pass in. I think they are being changed to just have Player.Draw() and Enemy.Draw(), in which case the Draw() function here can be adjusted accordingly.
    
    I'm having a tough time returning the gun, sword, or healthPotion as defined in Tile.h in the GetItem() function, but it seems necessary for the project. That needs to be fixed for final implementation.

--- Team Project/Tile.cpp
+++ Team Project/Tile.cpp
@@ -1,7 +1,9 @@
 #include "Tile.h"
+#include "itemManage.h"
 
 // Tile Implementation
 Tile::Tile(int x, int y, TileType type) : x(x), y(y), type(type) {}
+//Tile::Tile(int x, int y, TileType type, Item item) : x(x), y(y), type(type), item(item) {}
 
 void Tile::SetType(TileType newType)
 {
@@ -13,43 +15,84 @@ TileType Tile::GetType() const
     return type;
 }
 
+Item Tile::GetItem(void)
+{
+    switch(type)
+    {
+    case GUN:
+        return gun;
+    case HP:
+        return healthPotion;
+    case SWORD:
+        return sword;
+    default:
+        return;
+    }
+}
+
 void Tile::Draw() const
 {
     switch (type)
     {
         // TODO: Some of the cases may be removed and replaced as a png file in the future
         case WALL:
-            glColor3ub(0, 0, 0); // Black for walls
+        {
+            // Don't remove braces here - needed for png decode execution in switch/case statement
+            YsRawPngDecoder png;
+            png.Decode("WALL.png");
+            glRasterPos2i(x, y);
+            glDrawPixels(png.wid, png.hei, GL_RGBA, GL_UNSIGNED_BYTE, png.rgba);
             break;
+        }
         case PLAYER:
+            // player.Draw(), but I'm not sure which direction or what values to pass in here
             glColor3ub(0, 255, 0); // Green for player
+            // ToDo: Replace with Player character
             break;
         case ENEMY:
+            // enemy.Draw(), but I'm not sure which direction or what values to pass in here
             glColor3ub(255, 0, 0); // Red for enemy
+            // ToDo: Replace with Enemy character
             break;
-        case ITEM:
-            glColor3ub(0, 0, 255); // Blue for item
+        case GUN:
+            gun.Draw(x, y);
+            break;
+        case HP:
+            healthPotion.Draw(x, y);
+            break;
+        case SWORD:
+            sword.Draw(x, y);
             break;
         case DOOR:
-            glColor3ub(255, 215, 0); // Gold for door
+        {
+            YsRawPngDecoder png;
+            png.Decode("DOOR.png");
+            png.Flip();
+            glRasterPos2i(x, y);
+            glDrawPixels(png.wid, png.hei, GL_RGBA, GL_UNSIGNED_BYTE, png.rgba);
+            /*glColor3ub(255, 215, 0);*/ // Gold for door
             break;
+        }
         default:
             glColor3ub(255, 255, 255); // White for empty tiles
             break;
     }
 
-    glBegin(GL_QUADS);
-    glVertex2i(x, y);
-    glVertex2i(x + TILE_SIZE, y);
-    glVertex2i(x + TILE_SIZE, y + TILE_SIZE);
-    glVertex2i(x, y + TILE_SIZE);
-    glEnd();
+    if (type == PLAYER || type == ENEMY)
+    {
+        glBegin(GL_QUADS);
+        glVertex2i(x, y);
+        glVertex2i(x + TILE_SIZE, y);
+        glVertex2i(x + TILE_SIZE, y + TILE_SIZE);
+        glVertex2i(x, y + TILE_SIZE);
+        glEnd();
 
-    glColor3ub(200, 200, 200);
-    glBegin(GL_LINE_LOOP); // Draw border
-    glVertex2i(x + 1, y + 1);
-    glVertex2i(x + TILE_SIZE - 1, y + 1);
-    glVertex2i(x + TILE_SIZE - 1, y + TILE_SIZE - 1);
-    glVertex2i(x + 1, y + TILE_SIZE - 1);
-    glEnd();
+        glColor3ub(200, 200, 200);
+        glBegin(GL_LINE_LOOP); // Draw border
+        glVertex2i(x + 1, y + 1);
+        glVertex2i(x + TILE_SIZE - 1, y + 1);
+        glVertex2i(x + TILE_SIZE - 1, y + TILE_SIZE - 1);
+        glVertex2i(x + 1, y + TILE_SIZE - 1);
+        glEnd();
+    }
 }
