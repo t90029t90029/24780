7f06bf8fb3844264a614a0cb4586b0c9aeebd35f
PetrMundo <68665135+PetrMundo@users.noreply.github.com>
2024-12-03 22:50:13 -0500

    Tile update

--- Team Project/Tile.cpp
+++ Team Project/Tile.cpp
@@ -54,10 +54,43 @@ void Tile::Draw() const
     switch (baseType)
     {
     case TILE_WALL:
-        glColor3ub(0, 0, 0); // Black for walls
+    {
+        YsRawPngDecoder png;
+        if (YSOK == png.Decode("WALL.png"))
+        {
+            png.Flip();
+        }
+        
+
+        // This is to band-aid fix a rendering bug where these values are shifted
+        int posx = x; int posy = y;
+        if (x >= 440)
+        {
+            posx -= 1;
+        }
+        if (y >= 360)
+        {
+            posy -= 1;
+        }
+
+        glRasterPos2i(posx, posy + 40 - 1);
+        glDrawPixels(png.wid, png.hei, GL_RGBA, GL_UNSIGNED_BYTE, png.rgba);
+        goto LOOPOUT;
+    }
         break;
     case TILE_DOOR:
-        glColor3ub(255, 215, 0); // Gold for doors
+        //glColor3ub(255, 215, 0); // Gold for doors
+        {
+            YsRawPngDecoder png;
+            if (YSOK == png.Decode("DOOR.png"))
+            {
+                png.Flip();
+            }
+            if (x >= 440)
+            glRasterPos2i(x - 1, y + 40 - 1);
+            glDrawPixels(png.wid, png.hei, GL_RGBA, GL_UNSIGNED_BYTE, png.rgba);
+            goto LOOPOUT;
+        }
         break;
     case TILE_EMPTY:
     case TILE_HERO:
@@ -65,15 +98,21 @@ void Tile::Draw() const
     case TILE_ITEM:
     default:
         glColor3ub(255, 255, 255); // White for empty tiles
+        glBegin(GL_QUADS);
+        glVertex2i(x, y);
+        glVertex2i(x + 40, y);
+        glVertex2i(x + 40, y + 40);
+        glVertex2i(x, y + 40);
+        glEnd();
         break;
     }
 
-    glBegin(GL_QUADS);
-    glVertex2i(x, y);
-    glVertex2i(x + 40, y);
-    glVertex2i(x + 40, y + 40);
-    glVertex2i(x, y + 40);
-    glEnd();
+    //glBegin(GL_QUADS);
+    //glVertex2i(x, y);
+    //glVertex2i(x + 40, y);
+    //glVertex2i(x + 40, y + 40);
+    //glVertex2i(x, y + 40);
+    //glEnd();
 
     // Draw entities on top
     if (item)
@@ -87,5 +126,8 @@ void Tile::Draw() const
     if (hero)
     {
         hero->Draw();
-    }
+    }   
+LOOPOUT:
+    glEnd();
 }
+
